4c75e206b0a289fda100dfcdc39eb614a4f24a5b
hagon-desctop <hagons.l@gmail.com>
2020-02-27 21:08:10 +0900

    빈값 처리 모나드

--- src/comma.test.js
+++ src/comma.test.js
@@ -1,7 +1,12 @@
-function main({ number = 0, padBy = ',', padHow = 3 }) {
-  if (number === undefined) return 0;
+function main(...obj) {
+  if (Object.entries(obj).length === 0) return '0';
+  let { number, padBy, padHow } = obj[0];
+  number = number ? number : '0';
+  padBy = padBy ? padBy : ',';
+  padHow = padHow ? padHow : 3;
+
   if (isNaN(padHow)) return number;
-  if (isNaN(number)) return 0;
+  if (isNaN(number)) return '0';
 
   const getPadBy = String(padBy);
   const getPadHow = Number(padHow);
@@ -49,4 +54,6 @@ test('It will return.', () => {
   expect(main({ number: -1000 })).toEqual('-1,000');
   expect(main({ number: '-1000' })).toEqual('-1,000');
   expect(main({ number: '-100000' })).toEqual('-100,000');
+  expect(main({ number: 0 })).toEqual('0');
+  expect(main()).toEqual('0');
 });
